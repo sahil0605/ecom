16f0f3cd81d4f80011d1eb10ac1a9e50fcb2b302
sahil rajput <sahilsinghsolanki2460@gmail.com>
2023-12-13 22:56:48 +0530

    sm chng

--- client/src/components/item/Item.js
+++ client/src/components/item/Item.js
@@ -7,15 +7,29 @@ function Item() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const type = searchParams.get("type");
-
+  const [user, setUser] = useState(JSON.parse(window.localStorage.getItem("user")));
   const [products, setProducts] = useState([]);
+
+  const addCartHandler = async (itemId) => {
+    try {
+      // Make a POST request to add the item to the cart
+      const response = await axios.post("http://localhost:5000/api/user/addItemToCart", {
+        userId: user._id, // Assuming user._id is the userId
+        itemId: itemId,
+      });
+
+      console.log("Item added to cart:", response.data);
+    } catch (error) {
+      console.error("Error adding item to cart:", error);
+    }
+  };
 
   useEffect(() => {
     axios
       .get(`http://localhost:5000/api/user/getItem/${type}`)
       .then((response) => {
         console.log(response.data);
-        console.log(response.data.items);
+        console.log(response.data.items[0]._id);
         setProducts(response.data.items);
       })
       .catch((error) => console.error("Error fetching data:", error));
@@ -32,7 +46,7 @@ function Item() {
             <h3 className="product-name">{product.name}</h3>
             <p className="product-description">{product.description}</p>
             <p className="product-price">${product.price}</p>
-            <button>Add to cart</button>
+            <button onClick={() => addCartHandler(product._id)} >Add to cart</button>
           </div>
         </div>
       ))}
